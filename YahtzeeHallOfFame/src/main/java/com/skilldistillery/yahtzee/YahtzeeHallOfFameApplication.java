92e993a03b9f37d6ee0363170090bc9ae1de9797
Max McGregor <maxmcgregor1@gmail.com>
2022-01-07 16:40:38 -0600

    created spring boot project

---
+++ YahtzeeHallOfFame/src/main/java/com/skilldistillery/yahtzee/YahtzeeHallOfFameApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.yahtzee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YahtzeeHallOfFameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YahtzeeHallOfFameApplication.class, args);
+	}
+
+}
---
+++ YahtzeeHallOfFame/src/test/java/com/skilldistillery/yahtzee/YahtzeeHallOfFameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.yahtzee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YahtzeeHallOfFameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

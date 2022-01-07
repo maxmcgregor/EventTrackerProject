814c57868ae85abd7d566101f0325a2783cc2913
Max McGregor <maxmcgregor1@gmail.com>
2022-01-07 16:34:11 -0600

    built out game entity & junit test

---
+++ JPAYahtzee/src/main/java/com/skilldistillery/yahtzee/entities/Game.java
@@ -0,0 +1,62 @@
+package com.skilldistillery.yahtzee.entities;
+
+import java.util.Objects;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Game {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	private int score;
+
+	public Game() {
+		super();
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public int getScore() {
+		return score;
+	}
+
+	public void setScore(int score) {
+		this.score = score;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Game other = (Game) obj;
+		return id == other.id;
+	}
+
+	@Override
+	public String toString() {
+		return "Game [id=" + id + ", score=" + score + "]";
+	}
+	
+	
+}
